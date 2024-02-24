c8c8c127830ac419309bb1ef691a78ae73c373ef
robinsr <ryan.b.robinson@gmail.com>
2024-02-23 20:16:04 -0800

    removed unused stuff

--- system/path.sh
+++
@@ -1,3 +0,0 @@
-export PATH="/usr/local/share/npm/bin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$HOME/dotfiles/bin:$HOME/bin:$PATH"
-
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
